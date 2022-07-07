0c0332e5fdd3ab66adba03c7e2000132c3706459
Worldinova <worldinovamanufacturers@gmail.com>
2022-07-07 20:32:03 +0530

    1.0.0

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+    "use strict";
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.header').addClass('header-sticky');
+        } else {
+            $('.header').removeClass('header-sticky');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+
+})(jQuery);
+
